914d7b0ee10f71675c959a06f96b39ec81b77fe3
kalptaru1 <kalptarutech2025@gmail.com>
2025-03-26 15:28:46 +0530

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,4 @@
+# Weather Prediction Endpoint
+def get_weather_data():
+    # Dummy data for now, can be replaced with an API call
+    return {"forecast": "Sunny with light rain in the next 30 days"}
